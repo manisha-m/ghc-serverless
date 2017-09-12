684637388b9945d118be3bb596adec1f2e9ec3f7
manisha-m <manisha.malla@gmail.com>
2017-09-12 05:31:39 +0000

    Initial commit with baseline serverless.yml and handler.py for LMS usecase

---
+++ lmsService/handler.py
@@ -0,0 +1,46 @@
+import json
+import boto3
+import os
+
+def s3assignment(event, context):
+
+    print("In s3assignment, got event " + json.dumps(event))
+    
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    file_name = event['Records'][0]['s3']['object']['key']
+
+    print('Bucket', bucket_name, 'File', file_name)
+
+    sns_client = boto3.client('sns')
+
+    topic_arn = os.environ['sns_topic_arn'] 
+
+    message = "Assignment:" + file_name
+    pub_response = sns_client.publish(
+                                  TopicArn= topic_arn,
+                                  Message= message,
+                                  Subject="Alert"
+                                  )
+
+    print(pub_response)
+    response = {}
+    return response
+
+def notifier(event, context):
+
+    print("In notifier, event:" + json.dumps(event))
+    
+    # This piece of code gets details like course id and new assignment,
+    # and sends a notification email to the course Teacher about new assignment
+
+    response= {}
+    return response
+
+def updatedb(event, context):
+
+    print("In updatedb, event:" + json.dumps(event))
+    # This piece of code gets details like course id and new assignment,
+    # and updates the ToDo of the Teacher in the DynamoDB
+
+    response= {}
+    return response
