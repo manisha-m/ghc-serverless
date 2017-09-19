8059d13c64cff52c5126731c8f6723fdb1b0deef
manisha-m <manisha.malla@gmail.com>
2017-09-19 05:14:12 +0000

    Changed updatedb lambda to write to Teacher ToDo, uses old API params, needs to be fixed

--- lmsService/handler.py
+++ lmsService/handler.py
@@ -15,7 +15,7 @@ def s3assignment(event, context):
 
     topic_arn = os.environ['sns_topic_arn'] 
 
-    message = "Assignment:" + file_name
+    message = "Type:Assignment::Course:PyBasic::File:" + file_name
     pub_response = sns_client.publish(
                                   TopicArn= topic_arn,
                                   Message= message,
@@ -39,8 +39,43 @@ def notifier(event, context):
 def updatedb(event, context):
 
     print("In updatedb, event:" + json.dumps(event))
+
     # This piece of code gets details like course id and new assignment,
     # and updates the ToDo of the Teacher in the DynamoDB
+    message = event['Records'][0]['Sns']['Message']
+    print("In updatedb, printing message", message)
 
+    sub_msg = message.split('::')
+    msg_type = sub_msg[0].split(':')[1]
+    course = sub_msg[1].split(':')[1]
+    file_name = sub_msg[2].split(':')[1]
+
+    # Now get the Teacher for the course from the dynamodb
+    db_client = boto3.client('dynamodb')
+
+    c_item = db_client.get_item(
+                TableName='Courses',
+                Key={
+                    'CourseId': {'S': course}
+                    }
+            )
+    print("DB item received", json.dumps(c_item))
+    print(type(c_item['Item']['TeacherId']["N"]))
+
+    # Update the ToDo attribute for the course Teacher
+    updated_item = db_client.update_item(
+                TableName='Teacher',
+                Key={
+                    'Id': {'N': c_item['Item']['TeacherId']["N"]}
+                    },
+                AttributeUpdates={
+                    'ToDo': {
+                        'Value': {
+                            'SS': [ file_name]
+                            }
+                        }
+                    },
+                ReturnValues='UPDATED_NEW'
+            )
     response= {}
     return response
